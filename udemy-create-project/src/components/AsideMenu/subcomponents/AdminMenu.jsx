c768176c792de27fd4fdbbd0ce6598c1c732f59a
Marcin <marcinlacala@gmail.com>
2021-05-26 16:21:14 +0200

    Feat: Navigation of app

---
+++ udemy-create-project/src/components/AsideMenu/subcomponents/AdminMenu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import bemCssModules from "bem-css-modules";
+import { Link } from "react-router-dom";
+
+import { default as AsideMenuStyles } from "../AsideMenu.module.scss";
+
+const style = bemCssModules(AsideMenuStyles);
+
+const AdminMenu = ({ isUserLogged }) => (
+  <>
+    <p className={style("title")}>Panel Admina</p>
+    <nav>
+      <ul>
+        <li className={style("link")}>
+          <Link to="">Zarządzanie Marzeniami</Link>
+        </li>
+        {isUserLogged && (
+          <li className={style("link")}>
+            <Link to="/manage-courses">Moje spełnione marzenia</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  </>
+);
+
+export default AdminMenu;
